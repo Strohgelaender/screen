806fc17fdcbd2061e48d27bdac1e45f843c0c151
Lucas Welscher <ga53foy@mytum.de>
2025-02-05 12:47:50 +0100

    add repositories

---
+++ src/main/java/de/fll/screen/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package de.fll.screen.repository;
+
+import de.fll.screen.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {}
