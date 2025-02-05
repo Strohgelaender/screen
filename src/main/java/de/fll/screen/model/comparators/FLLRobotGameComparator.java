019234dfaa2658ef52466831458e54ece3c05dce
Lucas Welscher <ga53foy@mytum.de>
2025-02-05 12:17:30 +0100

    Automatic code formatting (basics)

--- src/main/java/de/fll/screen/model/comparators/FLLRobotGameComparator.java
+++ src/main/java/de/fll/screen/model/comparators/FLLRobotGameComparator.java
@@ -2,30 +2,29 @@ package de.fll.screen.model.comparators;
 
 import de.fll.screen.model.Score;
 import de.fll.screen.model.Team;
-
 import java.util.Comparator;
 import java.util.List;
 
 public class FLLRobotGameComparator implements Comparator<Team> {
 
-	@Override
-	public int compare(Team team1, Team team2) {
-		var t1Scores = getRelevantScores(team1.getScores());
-		var t2Scores = getRelevantScores(team2.getScores());
+    @Override
+    public int compare(Team team1, Team team2) {
+        var t1Scores = getRelevantScores(team1.getScores());
+        var t2Scores = getRelevantScores(team2.getScores());
 
-		t1Scores.sort(Comparator.comparing(Score::getPoints).reversed());
-		t2Scores.sort(Comparator.comparing(Score::getPoints).reversed());
+        t1Scores.sort(Comparator.comparing(Score::getPoints).reversed());
+        t2Scores.sort(Comparator.comparing(Score::getPoints).reversed());
 
-		for (int i = 0; i < t1Scores.size(); i++) {
-			if (t1Scores.get(i).getPoints() != t2Scores.get(i).getPoints()) {
-				return Double.compare(t2Scores.get(i).getPoints(), t1Scores.get(i).getPoints());
-			}
-		}
+        for (int i = 0; i < t1Scores.size(); i++) {
+            if (t1Scores.get(i).getPoints() != t2Scores.get(i).getPoints()) {
+                return Double.compare(t2Scores.get(i).getPoints(), t1Scores.get(i).getPoints());
+            }
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	private List<Score> getRelevantScores(List<Score> scores) {
-		return scores.subList(0, 3);
-	}
+    private List<Score> getRelevantScores(List<Score> scores) {
+        return scores.subList(0, 3);
+    }
 }
--- src/main/java/de/fll/screen/model/comparators/WRO2025Comparator.java
+++ src/main/java/de/fll/screen/model/comparators/WRO2025Comparator.java
@@ -2,47 +2,46 @@ package de.fll.screen.model.comparators;
 
 import de.fll.screen.model.Score;
 import de.fll.screen.model.Team;
-
 import java.util.Comparator;
 
 public class WRO2025Comparator implements Comparator<Team> {
 
-	@Override
-	public int compare(Team t1, Team t2) {
-		var t1Morning = getBestRoundMorning(t1);
-		var t2Morning = getBestRoundMorning(t2);
+    @Override
+    public int compare(Team t1, Team t2) {
+        var t1Morning = getBestRoundMorning(t1);
+        var t2Morning = getBestRoundMorning(t2);
 
-		var t1Afternoon = getBestRoundAfternoon(t1);
-		var t2Afternoon = getBestRoundAfternoon(t2);
+        var t1Afternoon = getBestRoundAfternoon(t1);
+        var t2Afternoon = getBestRoundAfternoon(t2);
 
-		Score t1Points = t1Morning.add(t1Afternoon);
-		Score t2Points = t2Morning.add(t2Afternoon);
+        Score t1Points = t1Morning.add(t1Afternoon);
+        Score t2Points = t2Morning.add(t2Afternoon);
 
-		int resultBoth = t1Points.compareToWithTime(t2Points);
-		if (resultBoth != 0) {
-			return resultBoth;
-		}
+        int resultBoth = t1Points.compareToWithTime(t2Points);
+        if (resultBoth != 0) {
+            return resultBoth;
+        }
 
-		return t1Morning.compareToWithTime(t2Morning);
-	}
+        return t1Morning.compareToWithTime(t2Morning);
+    }
 
-	private Score getBestRoundMorning(Team team) {
-		var s1 = team.getScores().get(0);
-		var s2 = team.getScores().get(1);
-		return getBetterRound(s1, s2);
-	}
+    private Score getBestRoundMorning(Team team) {
+        var s1 = team.getScores().get(0);
+        var s2 = team.getScores().get(1);
+        return getBetterRound(s1, s2);
+    }
 
-	private Score getBetterRound(Score s1, Score s2) {
-		if (s1.getPoints() > s2.getPoints()) {
-			return s1;
-		} else if (s1.getPoints() < s2.getPoints()) {
-			return s2;
-		} else {
-			return s1.getTime() < s2.getTime() ? s1 : s2;
-		}
-	}
+    private Score getBetterRound(Score s1, Score s2) {
+        if (s1.getPoints() > s2.getPoints()) {
+            return s1;
+        } else if (s1.getPoints() < s2.getPoints()) {
+            return s2;
+        } else {
+            return s1.getTime() < s2.getTime() ? s1 : s2;
+        }
+    }
 
-	private Score getBestRoundAfternoon(Team team) {
-		return getBetterRound(team.getScores().get(2), team.getScores().get(3));
-	}
+    private Score getBestRoundAfternoon(Team team) {
+        return getBetterRound(team.getScores().get(2), team.getScores().get(3));
+    }
 }
