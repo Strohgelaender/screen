174525b346a5b5da39d0bf0d5bffb6e2a1c9d6ab
Lucas Welscher <ga53foy@mytum.de>
2025-02-12 20:34:48 +0100

    Add some logic to calculate teams per page

--- src/main/webapp/app/screen/page.tsx
+++ src/main/webapp/app/screen/page.tsx
@@ -10,6 +10,7 @@ export default function ScoreScreenPage() {
     const [error, setError] = useState<string | null>(null);
     const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [teamsPerPage, setTeamsPerPage] = useState(8);
 
     useEffect(() => {
         fetch("http://localhost:8080/images/Hintergrund.png")
@@ -21,15 +22,33 @@ export default function ScoreScreenPage() {
     useEffect(() => {
         fetch("http://localhost:8080/api/parse")
             .then((response) => response.json())
-            .then(setCompetition)
+            .then((competition) => {
+                setCompetition(competition);
+                const teams = competition.categories[0].teams;
+                let pages = 3;
+                if (teams.length < 8) {
+                    pages = 1;
+                } else if (teams.length < 16) {
+                    pages = 2;
+                }
+                let perPage = Math.ceil(teams.length / pages);
+                let teamsLastPage = teams.length % perPage;
+
+                while (teamsLastPage > 0 && teamsLastPage < 4 && teams.length > 8) {
+                    // To avoid a page with less than 4 teams, we reduce the number of teams per page
+                    perPage--;
+                    teamsLastPage = teams.length % perPage;
+                }
+                setTeamsPerPage(perPage);
+            })
             .catch((error) => setError(error.message));
     }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => {
-                if (!competition || prevIndex + 8 > competition.categories[0].teams.length) return 0;
-                return (prevIndex + 8) % competition.categories[0].teams.length;
+                if (!competition || prevIndex + teamsPerPage > competition.categories[0].teams.length) return 0;
+                return (prevIndex + teamsPerPage) % competition.categories[0].teams.length;
             });
         }, 15000);
 
@@ -38,7 +57,8 @@ export default function ScoreScreenPage() {
 
     function renderScoreCell(score: Score, index: number) {
         const background = score.highlight ? 'blue' : 'none';
-        return <td className="px-4 border-t border-r border-l border-white text-center" key={index} style={{ background }}>{score.points}</td>;
+        return <td className="px-4 border-t border-r border-l border-white text-center" key={index}
+                   style={{background}}>{score.points}</td>;
     }
 
     const teams = competition?.categories[0].teams;
@@ -65,7 +85,7 @@ export default function ScoreScreenPage() {
                     </tr>
                     </thead>
                     <tbody>
-                    {teams?.slice(currentIndex, Math.min(currentIndex + 8, teams?.length)).map((team: Team) => (
+                    {teams?.slice(currentIndex, Math.min(currentIndex + teamsPerPage, teams?.length)).map((team: Team) => (
                         <tr key={team.id}>
                             <td className="px-4 py-2 border-t border-white">{team.name}</td>
                             {team.scores.map((score, index) => renderScoreCell(score, index))}
