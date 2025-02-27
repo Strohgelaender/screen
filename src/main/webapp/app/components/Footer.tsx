c9c32215f14b840f0b86c0bc1afc06096d521e38
Lucas Welscher <ga53foy@mytum.de>
2025-02-27 16:22:08 +0100

    prepare Regensburg

--- src/main/webapp/app/components/Footer.tsx
+++ src/main/webapp/app/components/Footer.tsx
@@ -11,9 +11,9 @@ const BASE_URL = "http://localhost:8080";
 const Footer: React.FC<FooterProps> = ({ settings }) => {
     return (
         settings?.showFooter &&
-        <footer className="bg-white w-full flex justify-evenly items-center" style={{ height: "15vh", position: "absolute", bottom: 0 }} id="screenFooter">
+        <footer className="bg-white w-full flex justify-around items-center" style={{ height: "15vh", position: "absolute", bottom: 0 }} id="screenFooter">
             {settings?.footerImages.map((image) => (
-                <div key={image} className="h-18">
+                <div key={image} className="h-18 flex-1 flex justify-center px-8">
                     <img src={BASE_URL + image} alt="" style={{ maxHeight: "13vh" }} />
                 </div>
             ))}
